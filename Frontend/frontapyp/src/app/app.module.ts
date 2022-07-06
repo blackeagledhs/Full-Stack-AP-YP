25618fb264200f00411225cf8907aa3e74c06b94
Mauro Vicens <blackeagledhs@gmail.com>
2022-07-06 08:34:58 -0300

    Solucionado problema de carga del backend

--- Frontend/frontapyp/src/app/app.module.ts
+++ Frontend/frontapyp/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -31,7 +32,8 @@ import { TopbarComponent } from './page/topbar/topbar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
